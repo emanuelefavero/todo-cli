5ea48226e06469fe51343519b1a9537594bcc312
Emanuele Favero <emafaux89@gmail.com>
2025-05-29 22:58:35 +0200

    refactor: move command handler into separate file

---
+++ src/commands.rs
@@ -0,0 +1,62 @@
+use crate::data;
+use crate::view;
+
+// * Command line argument handler
+pub fn handler(args: Vec<String>) {
+    match args.len() {
+        1 => {
+            if let Err(e) = view::todos::all() {
+                eprintln!("Error: {}", e);
+            }
+        }
+        2 if args[1] == "help" => {
+            view::help::usage();
+        }
+        2 if args[1] == "clear" => {
+            if let Err(e) = data::todos::clear() {
+                eprintln!("Error: {}", e);
+            }
+        }
+        3 if args[1] == "add" => {
+            if let Err(e) = data::todos::add(&args[2]) {
+                eprintln!("Error: {}", e);
+            }
+        }
+        2 if args[1] == "rm" => {
+            // If no index is provided, remove the first todo
+            if let Err(e) = data::todos::remove(1) {
+                eprintln!("Error: {}", e);
+            }
+        }
+        3 if args[1] == "rm" => match args[2].parse::<usize>() {
+            Ok(index) => {
+                if let Err(e) = data::todos::remove(index) {
+                    eprintln!("Error: {}", e);
+                }
+            }
+            Err(_) => {
+                eprintln!("Invalid number: {}", args[2]);
+            }
+        },
+        2 if args[1] == "done" => {
+            // If no index is provided, toggle the first todo
+            if let Err(e) = data::todos::toggle(1) {
+                eprintln!("Error: {}", e);
+            }
+        }
+        3 if args[1] == "done" => match args[2].parse::<usize>() {
+            Ok(index) => {
+                if let Err(e) = data::todos::toggle(index) {
+                    eprintln!("Error: {}", e);
+                }
+            }
+            Err(_) => {
+                eprintln!("Invalid number: {}", args[2]);
+            }
+        },
+        _ => {
+            eprintln!("Invalid command");
+            view::help::usage();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+mod commands;
 mod data;
 mod models;
 mod utils;
@@ -9,61 +10,6 @@ fn main() {
     // * Collect command line arguments into a vector
     let args: Vec<String> = env::args().collect();
 
-    // * Command line argument handler
-    match args.len() {
-        1 => {
-            if let Err(e) = view::todos::all() {
-                eprintln!("Error: {}", e);
-            }
-        }
-        2 if args[1] == "help" => {
-            view::help::usage();
-        }
-        2 if args[1] == "clear" => {
-            if let Err(e) = data::todos::clear() {
-                eprintln!("Error: {}", e);
-            }
-        }
-        3 if args[1] == "add" => {
-            if let Err(e) = data::todos::add(&args[2]) {
-                eprintln!("Error: {}", e);
-            }
-        }
-        2 if args[1] == "rm" => {
-            // If no index is provided, remove the first todo
-            if let Err(e) = data::todos::remove(1) {
-                eprintln!("Error: {}", e);
-            }
-        }
-        3 if args[1] == "rm" => match args[2].parse::<usize>() {
-            Ok(index) => {
-                if let Err(e) = data::todos::remove(index) {
-                    eprintln!("Error: {}", e);
-                }
-            }
-            Err(_) => {
-                eprintln!("Invalid number: {}", args[2]);
-            }
-        },
-        2 if args[1] == "done" => {
-            // If no index is provided, toggle the first todo
-            if let Err(e) = data::todos::toggle(1) {
-                eprintln!("Error: {}", e);
-            }
-        }
-        3 if args[1] == "done" => match args[2].parse::<usize>() {
-            Ok(index) => {
-                if let Err(e) = data::todos::toggle(index) {
-                    eprintln!("Error: {}", e);
-                }
-            }
-            Err(_) => {
-                eprintln!("Invalid number: {}", args[2]);
-            }
-        },
-        _ => {
-            eprintln!("Invalid command");
-            view::help::usage();
-        }
-    }
+    // * Call the command handler
+    commands::handler(args);
 }
