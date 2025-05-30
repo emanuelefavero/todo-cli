6ee7ba4afb04871f6db7b231fe07788fd4839dc2
Emanuele Favero <emafaux89@gmail.com>
2025-05-30 04:41:47 +0200

    feat: style usage ouput titles

--- src/view/help.rs
+++ src/view/help.rs
@@ -1,13 +1,27 @@
+use colored::Colorize;
+
+// 📢 PUBLIC ----------------------------------
+
 // * Show the app usage instructions
 pub fn usage() {
-    println!("Usage:");
-    println!("  todo - List all todos");
-    println!("  todo add \"Todo text\" - Add a new todo");
-    println!("  todo add \"Todo text\" <number> - Add a new todo at specific position");
-    println!("  todo rm - Remove the first todo");
-    println!("  todo rm <number> - Remove a specific todo by number");
-    println!("  todo done - Toggle the first todo completion status");
-    println!("  todo done <number> - Toggle todo completion status");
-    println!("  todo clear - Remove all todos");
-    println!("  todo help - Show this help message");
+    println!("");
+    println!("{}", title("Usage:"));
+    println!("");
+    println!("{}", title("Commands:"));
+    println!("  todo                List all todos");
+    println!("  todo add \"text\"     Add a new todo");
+    println!("  todo add \"text\" <n> Add a new todo at specific position");
+    println!("  todo rm             Remove the first todo");
+    println!("  todo rm <n>         Remove a specific todo by number");
+    println!("  todo done           Toggle the first todo completion status");
+    println!("  todo done <n>       Toggle todo completion status");
+    println!("  todo clear          Remove all todos");
+    println!("  todo help           Show this help message");
+}
+
+// 🔒 PRIVATE ---------------------------------
+
+// ? Format title
+fn title(title: &str) -> colored::ColoredString {
+    title.bold().green()
 }
